ced7e283994ff6874f38b476ad6a0a845207eb94
yudi <satoto@investree.id>
2019-03-13 17:35:03 +0700

    init vue

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,19 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'space-in-parens': ['off'],
+    'quotes': ['error', 'single'],
+    'no-undef': ['off'],
+    'computed-property-spacing': ['off'],
+    'linebreak-style': ['off'],
+    'brace-style': [2, 'stroustrup', { allowSingleLine: false }],
+    'comma-dangle': ['error', 'never'],
+    'no-param-reassign': ['error', { props: false }],
+    'semi': ['error', 'never'],
+    'camelcase': ['error', { properties: 'always' }],
+    'function-paren-newline': ['off'],
+    'implicit-arrow-linebreak': ['off'],
+    'prefer-arrow-callback': ['off']
   },
   parserOptions: {
     parser: 'babel-eslint',
